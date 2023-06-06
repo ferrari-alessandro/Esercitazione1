c97e99a5232420003f70c6a50f8667fba4b26af2
Alessandro Ferrari <alessandro.ferrari@nttdata.com>
2023-06-06 09:17:32 +0200

    Aggiunta semplice form di login

---
+++ WebContent/login.jsp
@@ -0,0 +1,35 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ page import="java.util.*"%>
+<%@ page import="java.net.InetAddress"%>
+<%@ page session="true" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans"
+	rel="stylesheet">
+<title>Sample Web Application - SampleServlet</title>
+<style>
+body {
+	font-family: 'Open Sans', sans-serif;
+}
+
+table, td, tr {
+	border: 1px solid;
+	border-collapse: collapse;
+}
+
+span {
+	font-weight: normal;
+	font-size: 16px;
+	color: black;
+}
+</style>
+</head>
+<body>
+<h1> Pagina per l'utente loggato</h1>
+
+		
+		
+</body>
+</html>
---
+++ src/com/nttdata/LoginServlet.java
@@ -0,0 +1,94 @@
+package com.nttdata;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+public class LoginServlet extends HttpServlet {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            try {
+                
+            	 HttpSession session = request.getSession();
+            	 
+                String userName = request.getParameter("username");
+                String password = request.getParameter("password");
+                if(password.equalsIgnoreCase("admin")) {
+                	RequestDispatcher view = request.getRequestDispatcher("login.jsp");
+                    view.forward(request,response);	
+
+                }else {
+                	RequestDispatcher view = request.getRequestDispatcher("error.jsp");
+                    view.forward(request,response);	
+ 
+                }             	          
+            } finally {
+                out.close();
+            }
+        }
+
+    }
+ 
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "SampleServlet";
+    }
+
+}
